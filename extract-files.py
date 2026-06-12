38de9129be45ee0e27a49b05b4ce752f3fb7a13e
StaticOS <yusuf1905emre1905@gmail.com>
2026-06-12 14:27:24 +0300

    Add extract-files.py for StaticOS project

---
+++ extract-files.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2026 The StaticOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/samsung/sm8250-common',
+    'vendor/qcom/opensource/display',
+]
+
+module = ExtractUtilsModule(
+    'gts7lwifi',
+    'samsung',
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8250-common', module.vendor
+    )
+    utils.run()
+  
